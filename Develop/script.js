858b6c18f19600b491486c6781c198f4f70911b4
B-Litty <bradlitman94@gmail.com>
2021-09-17 15:22:24 -0700

    intial setup w/ user input and validation

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,69 @@
+
 // Assignment code here
+function generatePassword () {
+  // variable initialization
+  let passwordLength;
+  let uppercase;
+  let lowercase;
+  let special;
+
+
+  window.alert("We are going to ask you a series of questions to determine your perfect password");
+
+  // Validation for the password to ensure the user selected a range between 8-128
+  function passwordLengthValidation () {
+    passwordLength = window.prompt("How long would you like your password to be? Select between 8 - 128");
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength === false) {
+      window.alert("You must select a range between 8 - 128");
+      passwordLengthValidation();
+    } 
+  }
+  passwordLengthValidation();
+
+
+  // Validation to ensure at least one is selected
+  function passwordCustomizationValidation () {
+    uppercase = window.confirm("Would you like Capital Letters in your password?");
+    lowercase = window.confirm("Would you like lowercase letters in your password?");
+    numbers = window.confirm("Would you like numbers to be included in your password?");
+    special = window.confirm("Would you like special characters in your password?");
+
+    if (!uppercase && !lowercase && !numbers && !special) {
+      window.alert("You must select at least one requirement");
+      passwordCustomizationValidation();
+    }
+  }
+  passwordCustomizationValidation();
+
+
+
+}
+
+
+
+
+
+
+
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.getElementById("generate");
+let resetBtn = document.getElementById("reset")
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.getElementById("password");
   passwordText.value = password;
+}
 
+// Additional reset password button aded
+function resetPassword () {
+  document.getElementById('password').innerHTML = null;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+resetBtn.addEventListener('click', resetPassword)
